e025f4ed6cedc8c6a1599b65e52bbee13f1be22b
enhuiz <niuzhe.nz@outlook.com>
2021-09-08 12:54:28 +0800

    more options

--- tzq_cvae/models.py
+++ tzq_cvae/models.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 from torch.distributions import Normal, kl_divergence
-from torchzq.named_array import NamedArray
-from operator import itemgetter
 
 from .utils import ModuleTraits
 
@@ -18,27 +16,41 @@ class Baseline(nn.Sequential):
         )
 
 
-class Encoder(ModuleTraits, Baseline):
+class EncoderTraits(ModuleTraits):
+    @property
+    def normal(self) -> Normal:
+        return self._saved_for_later["normal"]
+
+
+class Encoder(EncoderTraits, Baseline):
     def __init__(self):
         super().__init__(dim_out=400)
 
-    def forward(self, x, y):
-        x = x.clone()
-        x[x == -1] = y[x == -1]
+    def forward(self, x, y_=None):
+        if y_ is not None:
+            x = x.clone()
+            x[x == -1] = y_[x == -1]
         μ, logσ = super().forward(x).chunk(2, dim=-1)
         σ = logσ.exp()
         z = μ + σ * torch.randn_like(μ)
         self.save_for_later(normal=Normal(μ, σ))
         return z
 
-    @property
-    def normal(self) -> Normal:
-        return self._saved_for_later["normal"]
+
+class DumbEncoder(EncoderTraits, nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, _):
+        μ = torch.zeros(len(x), 200, device=x.device)
+        normal = Normal(μ, torch.ones_like(μ))
+        self.save_for_later(normal=normal)
+        return normal.sample()
 
 
 class Decoder(Baseline):
-    def __init__(self):
-        super().__init__(dim_in=200)
+    def __init__(self, conditioned=False):
+        super().__init__(dim_in=984 if conditioned else 200)
 
 
 if __name__ == "__main__":
--- tzq_cvae/runners/cvae.py
+++ tzq_cvae/runners/cvae.py
@@ -7,7 +7,7 @@ from pathlib import Path
 from typing import Optional
 from einops import rearrange, repeat
 
-from ..models import Baseline, Encoder, Decoder, kl_divergence
+from ..models import Baseline, Encoder, DumbEncoder, Decoder, kl_divergence
 from .baseline import Runner as BaselineRunner
 
 
@@ -22,6 +22,9 @@ class Runner(BaselineRunner):
         sample_from_prior_when_training: bool = False,
         freeze_baseline: bool = True,
         importance_sampling_s: int = 100,
+        conditioned_decoder: bool = False,
+        conditioned_prior: bool = True,
+        use_baseline: bool = True,
         **kwargs
     ):
         super().__init__(**kwargs)
@@ -29,36 +32,75 @@ class Runner(BaselineRunner):
     def create_model(self):
         args = self.args
 
-        self.baseline = Baseline()
+        if self.use_baseline:
+            self.baseline = Baseline()
 
-        if args.baseline_ckpt is not None:
-            self.baseline.load_state_dict(
-                torch.load(args.baseline_ckpt, "cpu")["model"]
-            )
+            if args.baseline_ckpt is not None:
+                self.baseline.load_state_dict(
+                    torch.load(args.baseline_ckpt, "cpu")["model"]
+                )
 
-        if args.freeze_baseline:
-            for p in self.baseline.parameters():
-                p.requires_grad_(False)
+            if args.freeze_baseline:
+                for p in self.baseline.parameters():
+                    p.requires_grad_(False)
 
-        self.prior = Encoder()
+        self.prior = Encoder() if args.conditioned_prior else DumbEncoder()
         self.encoder = Encoder()
-        self.decoder = Decoder()
+        self.decoder = Decoder(args.conditioned_decoder)
 
-        return nn.ModuleDict(
-            dict(
-                baseline=self.baseline,
-                prior=self.prior,
-                encoder=self.encoder,
-                decoder=self.decoder,
-            ),
+        mapping = dict(
+            prior=self.prior,
+            encoder=self.encoder,
+            decoder=self.decoder,
         )
+
+        if args.use_baseline:
+            mapping["baseline"] = self.baseline
+
+        return nn.ModuleDict(mapping)
 
     @torch.no_grad()
     def generate(self, x):
+        args = self.args
         x = self.flatten(x)
-        z_prior = self.prior(x, self.baseline(x))
-        logits = self.decoder(z_prior)
+        z = self.prior(x, self.baseline(x) if self.use_baseline else None)
+        if args.conditioned_decoder:
+            z = torch.cat([z, x], dim=-1)
+        logits = self.decoder(z)
         return self.unflatten(logits).sigmoid()
+
+    def compute_ncll(self, x, y):
+        args = self.args
+
+        z = self.encoder.normal.sample((args.importance_sampling_s,))
+
+        logr = self.prior.normal.log_prob(z) - self.encoder.normal.log_prob(z)
+        logr = rearrange(logr.sum(dim=-1), "s b -> (s b)")
+
+        z = rearrange(z, "s b d -> (s b) d")
+        y = repeat(y, "b ... -> (s b) ...", s=args.importance_sampling_s)
+        unmasked = repeat(x != -1, "b ... -> (s b) ...", s=args.importance_sampling_s)
+
+        if args.conditioned_decoder:
+            x = self.flatten(x)
+            x = repeat(x, "b d -> (s b) d", s=args.importance_sampling_s)
+            z = torch.cat([z, x], dim=-1)
+
+        logits = self.unflatten(self.decoder(z))
+
+        bce = F.binary_cross_entropy_with_logits(logits, y, reduction="none")
+        bce[unmasked] = 0  # consider only masked pixels
+        bce = bce.flatten(1).sum(dim=-1)
+
+        logp = rearrange(logr - bce, "(s b) -> s b", b=len(x))
+        ncll = logmeanexp(logp, dim=0).neg().mean().item()
+
+        return ncll
+
+    @property
+    def use_baseline(self):
+        args = self.args
+        return args.use_baseline and args.conditioned_prior
 
     def training_step(self, batch, _):
         args = self.args
@@ -69,7 +111,7 @@ class Runner(BaselineRunner):
 
         z_posterior = self.encoder(x, y)
 
-        z_prior = self.prior(x, self.baseline(x))
+        z_prior = self.prior(x, self.baseline(x) if self.use_baseline else None)
 
         if not args.sample_from_prior_when_training and self.model.training:
             z = z_posterior
@@ -78,6 +120,9 @@ class Runner(BaselineRunner):
 
         kl = kl_divergence(self.encoder.normal, self.prior.normal)
         loss_kl = kl.mean(dim=0).sum()
+
+        if args.conditioned_decoder:
+            z = torch.cat([z, x], dim=-1)
 
         logits = self.unflatten(self.decoder(z))
 
@@ -93,22 +138,7 @@ class Runner(BaselineRunner):
         stat_dict = dict(loss_bce=loss_bce.item(), loss_kl=loss_kl.item())
 
         if not self.model.training:
-            z = self.encoder.normal.sample((args.importance_sampling_s,))
-
-            logr = self.prior.normal.log_prob(z) - self.encoder.normal.log_prob(z)
-            logr = rearrange(logr.sum(dim=-1), "s b -> (s b)")
-
-            z = rearrange(z, "s b d -> (s b) d")
-            y = repeat(y, "b ... -> (s b) ...", s=args.importance_sampling_s)
-            logits = self.unflatten(self.decoder(z))
-
-            bce = F.binary_cross_entropy_with_logits(logits, y, reduction="none")
-            masked = repeat(masked, "b ... -> (s b) ...", s=args.importance_sampling_s)
-            bce[~masked] = 0  # consider only masked pixels
-            bce = bce.flatten(1).sum(dim=-1)
-
-            logp = rearrange(logr - bce, "(s b) -> s b", b=len(x))
-            stat_dict["ncll"] = logmeanexp(logp, dim=0).neg().mean().item()
+            stat_dict["ncll"] = self.compute_ncll(x, y)
 
         return loss, stat_dict
 
